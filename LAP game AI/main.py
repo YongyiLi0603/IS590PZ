42a509469ccc1ad0576437337014bd131be249a1
Pupilsch <821058047@qq.com>
2019-10-04 10:31:22 -0500

    func choose_map

---
+++ LAP game AI/main.py
@@ -0,0 +1,47 @@
+import random
+
+def random_choose(union_set): #sub_fuction of choose_map. union_set is not empty
+    l=len(union_set)
+    ran=random.randint(0,l-1)
+    return union_set[ran]
+
+def choose_map(maps): #search all maps(n*n str), return a coordinate(m,n) and a set of union coordinate
+    length=len(maps)
+    union_1=[]
+    union_2=[]
+    union_3=[]
+    union_4=[]
+    for i in range(length-1):
+        for j in range(length-1):
+            union_count=0
+            sol=[[i,j]]
+            for x in range(2):
+                for y in range(2):
+                    if maps[i+x][j+y]!='0':
+                        union_count+=1
+                        sol.append([i+x,j+y])
+            if union_count==1:
+                union_1.append(sol)
+            if union_count==2:
+                union_2.append(sol)
+            if union_count==3:
+                union_3.append(sol)
+            if union_count==4:
+                union_4.append(sol)
+
+    if union_2:
+        s=random_choose(union_2)
+    elif union_1:
+        s=random_choose(union_1)
+    elif union_3:
+        s=random_choose(union_3)
+    elif union_4:
+        s=random_choose(union_4)
+    else: s=[]
+
+    return s
+
+if __name__ == "__main__":
+    maps=["000000","000xx0","00xxy0","0yyy00","0yy000","000000"]
+    peep=choose_map(maps) #A set of several coordinates(like [[0,1],[1,1],[1,2]]). peep[0] indicates the 2*2 matrix location and others are points that union
+                    
